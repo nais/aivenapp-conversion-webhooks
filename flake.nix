9419e6eeda2868217ec79d10f24387175da04df5
carl hedgren <carl.hedgren@nav.no>
2025-11-06 10:41:49 +0100

    all the deps

--- flake.nix
+++ flake.nix
@@ -144,7 +144,7 @@
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
-            # pkgs.ripgrep
+            pkgs.rust-analyzer
           ];
         };
       }
